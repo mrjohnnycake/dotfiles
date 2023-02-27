04c8da7f2a41d34f30f76d210fd35698f737dc42
Dave Davison <103395689+mrjohnnycake@users.noreply.github.com>
2023-02-26 17:37:20 -0800

    added .scripts/setup/

---
+++ .scripts/setup/step-one.sh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.config/zsh/powerlevel10k
+
+rm ~/.zshrc
+rm ~/.zcompdump
+rm ~/.bash_history
+rm ~/.bash_logout
+rm ~/.bashrc
